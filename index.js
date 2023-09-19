45c540cd2190fb6ccb0fc3f6443bda03367679f5
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-19 14:25:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location) {
+  if (location > 42) {
+    const numberOfBlocks = location - 42;
+    return numberOfBlocks;
+  } else {
+    const numberOfBlocks = 42 - location;
+    return numberOfBlocks;
+  }
+}
+
+function distanceFromHqInFeet(location) {
+  const distanceInFeet = distanceFromHqInBlocks(location) * 264;
+  return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, finish) {
+  const distanceInFeet = Math.abs((finish - start) * 264);
+  return distanceInFeet;
+}
+
+function calculatesFarePrice(start, finish) {
+  const totalDistance = distanceTravelledInFeet(start, finish);
+
+  if (totalDistance <= 400) {
+    const price = 0;
+    return price;
+  } else if (totalDistance <= 2000) {
+    const price = (totalDistance - 400) * 0.02;
+    return price;
+  } else if (totalDistance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
